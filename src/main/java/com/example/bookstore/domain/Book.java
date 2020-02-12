2380118f323dbb0d54ed1abaaa1f09d3f7989829
igor-3 <o.stenroos@gmail.com>
2020-02-12 22:42:13 +0200

    First commit

---
+++ src/main/java/com/example/bookstore/domain/Book.java
@@ -0,0 +1,24 @@
+package com.example.bookstore.domain;
+
+import javax.persistence.Entity;
+
+@Entity
+public class Book {
+	
+	private String title;
+	private String author;
+	private int year;
+	private String isbn;
+	private float price;
+	
+	public Book(String title, String author, int year, String isbn, float price) {
+		
+		this.title = title;
+		this.author = author;
+		this.year = year;
+		this.isbn = isbn;
+		this.price = price;
+		
+	}
+
+}
