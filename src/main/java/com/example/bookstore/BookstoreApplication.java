9e6e080524fee6b0e37d5104dd81c1cbc07d6ea2
igor-3 <o.stenroos@gmail.com>
2020-02-13 03:50:45 +0200

    Basic CRUD functionality added

--- src/main/java/com/example/bookstore/BookstoreApplication.java
+++ src/main/java/com/example/bookstore/BookstoreApplication.java
@@ -1,13 +1,38 @@
 package com.example.bookstore;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import com.example.bookstore.domain.Book;
+import com.example.bookstore.domain.BookRepository;
+
 
 @SpringBootApplication
 public class BookstoreApplication {
+	
+	private static final Logger log = LoggerFactory.getLogger(BookstoreApplication.class);
 
 	public static void main(String[] args) {
 		SpringApplication.run(BookstoreApplication.class, args);
+	}
+	
+	@Bean
+	public CommandLineRunner studentDemo(BookRepository repository) {
+		return (args) -> {
+			log.info("save a couple of books");
+			Book finnskogen = new Book("Finnskogen, elämän kehto", "Britt Karin Larsen", 2018, "978-952-312-696-1", 29.90);
+			Book scythians = new Book("The Scythians", "Barry Cunliffe", 2019, "978-0-19-882012-3", 34.90);	
+			repository.save(finnskogen);
+			repository.save(scythians);
+			scythians.setPrice(29.90);
+			scythians.setTitle("The Scythians - Nomad Warriors of the Steppe");
+			repository.save(scythians);
+
+		};
 	}
 
 }
--- src/main/java/com/example/bookstore/domain/Book.java
+++ src/main/java/com/example/bookstore/domain/Book.java
@@ -1,17 +1,25 @@
 package com.example.bookstore.domain;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
 @Entity
 public class Book {
 	
+	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
+    private Long id;
 	private String title;
 	private String author;
 	private int year;
 	private String isbn;
-	private float price;
+	private double price;
 	
-	public Book(String title, String author, int year, String isbn, float price) {
+	public Book() {}
+	
+	public Book(String title, String author, int year, String isbn, double price) {
 		
 		this.title = title;
 		this.author = author;
@@ -20,5 +28,55 @@ public class Book {
 		this.price = price;
 		
 	}
+	
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getAuthor() {
+		return author;
+	}
+
+	public void setAuthor(String author) {
+		this.author = author;
+	}
+
+	public int getYear() {
+		return year;
+	}
+
+	public void setYear(int year) {
+		this.year = year;
+	}
+
+	public String getIsbn() {
+		return isbn;
+	}
+
+	public void setIsbn(String isbn) {
+		this.isbn = isbn;
+	}
+
+	public double getPrice() {
+		return price;
+	}
+
+	public void setPrice(double price) {
+		this.price = price;
+	}
+
+
 
 }
