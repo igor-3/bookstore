9420b28868a156108be0e8fe4430d174345c7047
igor-3 <o.stenroos@gmail.com>
2020-03-05 15:28:51 +0200

    REST API improved

--- src/main/java/com/example/bookstore/domain/BookRepository.java
+++ src/main/java/com/example/bookstore/domain/BookRepository.java
@@ -1,8 +1,17 @@
 package com.example.bookstore.domain;
 
+import java.util.List;
+
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
-
+@RepositoryRestResource
 public interface BookRepository extends CrudRepository<Book, Long> {
+	List<Book> findByTitle(@Param("title") String title);
+	List<Book> findByAuthor(@Param("author") String author);
+	List<Book> findByYear(@Param("year") int year);
+	List<Book> findByIsbn(@Param("isbn") String isbn);
+	List<Book> findByPrice(@Param("price") double price);
 
 }
