e6237106a7dcf800e3971929d6e0ffcff45a1e53
igor-3 <o.stenroos@gmail.com>
2020-03-05 15:13:50 +0200

    REST API added

--- src/main/java/com/example/bookstore/web/BookController.java
+++ src/main/java/com/example/bookstore/web/BookController.java
@@ -1,11 +1,15 @@
 package com.example.bookstore.web;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.example.bookstore.domain.Book;
 import com.example.bookstore.domain.BookRepository;
@@ -25,6 +29,16 @@ public class BookController {
     public String bookList(Model model) {	
         model.addAttribute("books", repository.findAll());
         return "booklist";
+    }
+    
+    @RequestMapping(value="books",  method = RequestMethod.GET)
+    public @ResponseBody List<Book> bookListRest() {
+    	return (List<Book>) repository.findAll();
+    }
+    
+    @RequestMapping(value="/book/{id}", method = RequestMethod.GET)
+    public @ResponseBody Optional<Book> findBookRest(@PathVariable("id") Long bookId) {
+    	return repository.findById(bookId);
     }
 	
     @RequestMapping(value = "/add")
